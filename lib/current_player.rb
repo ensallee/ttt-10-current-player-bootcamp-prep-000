5f21d5c51fc6a2c4d6a5c9d993e9146a54789c0c
Betsy Sallee <ensallee@gmail.com>
2018-02-24 20:28:30 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,12 @@
 def turn_count(board)
   counter=0
   board.each do |turn|
-    if turn != " "
+    if turn != " " || ""
       counter += 1
     end
   end
   return counter
+end
+
+def current_player
 end
